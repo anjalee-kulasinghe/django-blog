13f05d8c0cd5435f8e4aa6d474a5f030ef214feb
Anjalee Kulasinghe <anjaleek@gmail.com>
2024-03-16 10:20:29 +0000

    add the new class comment

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 class Event(models.Model):
     event_name = models.CharField(max_length=200, unique=True)
     location = models.CharField(max_length=200)
@@ -39,3 +45,19 @@ class Ticket(models.Model):
 
     def __str__(self):
         return f"Ticket for {self.ticket_holder}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-anjaleekulas-djangoblog-f8rjg3u82qx.ws-eu108.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-anjaleekulas-djangoblog-f8rjg3u82qx.ws-eu109.gitpod.io']
 
 
 # Application definition
